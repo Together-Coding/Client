3df58e76dddcbcc6e1b4b32f9c73373e9c6f68cd
kwon <tnsdyd10@naver.com>
2022-06-23 15:55:13 +0900

    style: 약간의 HOME UI 조정

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,13 +1,10 @@
 import React from "react";
-import { Link, Switch, Route } from "react-router-dom";
-import { useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "../styles/Home.scss";
 import HOME_IMAGE from "../images/home-image.png";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 function Home() {
-  console.log(localStorage.getItem("access_token"));
-  const history = useHistory();
   const logOutCtrl = () => {
     if (window.confirm("로그아웃 하시겠습니까?")) {
       localStorage.removeItem("access_token");
@@ -24,25 +21,31 @@ function Home() {
         </p>
       </div>
       <div className="home-container">
-        
-        <img src={HOME_IMAGE} alt="home" />
-        
+        <div className="home-image-and-desc">
+          <img src={HOME_IMAGE} alt="home" />
+
+          <p>오프라인 코딩 수업의 문제점을 극복하기 위한</p>
+          <p style={{ fontWeight: "bold" }}>실시간 코드 공유 웹 서비스</p>
+        </div>
+
         <div className="login-info-container">
           {localStorage.getItem("access_token") !== null ? (
             <>
               <span className="home-my-class-info">
                 <Link to="/me">내 강의 정보</Link>
               </span>
-              <button className="home-logout-btn" onClick={logOutCtrl}>로그아웃</button>
+              <button className="home-logout-btn" onClick={logOutCtrl}>
+                로그아웃
+              </button>
             </>
           ) : (
             <>
-            
               <span className="login-logout">
                 <Link to="/user/login">Together-Coding 로그인</Link>
               </span>
               <span className="register">
-              <FontAwesomeIcon icon={faUser} /><Link to="/user/register">회원가입</Link>
+                <FontAwesomeIcon icon={faUser} />
+                <Link to="/user/register">회원가입</Link>
               </span>
             </>
           )}
